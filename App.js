b20cb3e91cc78bf989ac84c52dd9f44c8617a2c2
Son0108 <nguyentatson12346789@gmail.com>
2022-11-27 15:23:18 +0700

    create app

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Welcome from './src/FontEnd/Welcome/Welcome';
+
+export default function App() {
+  return (<Welcome/>)
+}
---
+++ src/FontEnd/Welcome/Welcome.js
@@ -0,0 +1,77 @@
+/*eslint-disable*/
+import React from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity, ImageBackground, SafeAreaView, TextInput} from 'react-native';
+
+function Welcome() {
+  return (
+    <ImageBackground style={{flex: 1,justifyContent: "center",alignItems: 'center'}} source={require('../assets/Images/background.jpg')}>
+        <SafeAreaView style={{flex: 1,width: '100%',}}>
+
+            <View style={styles.header}>
+              <Image style={{width: 200,height: 200}} source={require('../assets/Images/logo.png')}></Image>
+              <Text style={{color: '#990033', fontSize: 40}}>Thương mại điện tử</Text>
+            </View>
+
+            <View style={styles.content}>
+              <Text style={{color: '#fff', fontSize: 15, fontWeight: 'bold'}}>Tên đăng nhập</Text>
+              <View style={{backgroundColor:'#AB7082', marginTop: 4, borderRadius: 10, padding: 10, width:'70%', height: 40, flexDirection:'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                  <Image style={{display: 'flex',height:20, width: 20}} source={require('../assets/Images/user.png')}/>
+                  <TextInput
+                  style={{height:'100%', width:'90%'}}
+                  />
+              </View>
+              <Text style={{marginTop: 20, color: '#fff', fontSize: 15, fontWeight: 'bold'}}>Mật khẩu</Text>
+              <View style={{backgroundColor:'#AB7082', marginTop: 4, borderRadius: 10, padding: 10, width:'70%', height: 40, flexDirection:'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                <Image style={{display: 'flex',height:20, width: 20}} source={require('../assets/Images/password.png')}/>
+                <TextInput
+                style={{height:'100%', width:'90%'}}
+                />
+              </View>
+            </View>
+
+            <View style={styles.footer}>
+            <TouchableOpacity style={styles.btnLogin}>
+                <Text style={{color: '#fff',fontWeight: 'bold',fontSize: 20,}}>ĐĂNG NHẬP</Text>
+            </TouchableOpacity>
+            <Text style={{color: '#fff',fontSize: 14}}>Bạn chưa có tài khoản?
+                <TouchableOpacity>
+                <Text style={{color: '#fff',fontSize: 14}}>Đăng kí</Text>
+                </TouchableOpacity>
+            </Text>
+            </View>
+        </SafeAreaView >
+        </ImageBackground>
+ )}
+
+ const styles = StyleSheet.create({
+  header: {
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+  },
+  content: {
+    width: '100%',
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  footer : {
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center'
+  },
+  btnLogin: {
+      borderColor: '#AB7082',
+      backgroundColor: '#AB7082',
+      height: 50,
+      width: '63%',
+      borderRadius: 25,
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      marginBottom: 40
+  }});
+
+export default Welcome
